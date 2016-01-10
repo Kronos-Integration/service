6e08dbc836ca6c004f06d463e2654cf9c27648e4
Markus Felten <Markus.Felten@gmx.de>
2016-01-10 21:00:43 +0100

    fix(state): StateTransitionMixin now works

--- tests/service_test.js
+++ tests/service_test.js
@@ -9,41 +9,28 @@ const chai = require('chai'),
   should = chai.should(),
   Service = require('../lib/service');
 
-describe('service', function () {
+describe('service', () => {
   const s1 = new Service({
     key1: "value1",
     key2: 2
   });
 
-  describe('plain creation', function () {
-    it('has a type', function () {
-      assert.equal(s1.type, 'service');
-    });
-
-    it('has a toString', function () {
-      assert.equal(s1.toString(), 'service');
-    });
-
-    it('is stopped', function () {
-      assert.equal(s1.state, 'stopped');
-    });
-
-    it('has default logLevel info', function () {
-      assert.equal(s1.logLevel, 'info');
-    });
+  describe('plain creation', () => {
+    it('has a type', () => assert.equal(s1.type, 'service'));
+    it('has a toString', () => assert.equal(s1.toString(), 'service'));
+    it('is stopped', () => assert.equal(s1.state, 'stopped'));
+    it('has default logLevel info', () => assert.equal(s1.logLevel, 'info'));
   });
 
-  describe('creation with logLevel', function () {
+  describe('creation with logLevel', () => {
     const s2 = new Service({
       key1: "value1",
       logLevel: "trace"
     });
 
-    it('has given logLevel', function () {
-      assert.equal(s2.logLevel, 'trace');
-    });
+    it('has given logLevel', () => assert.equal(s2.logLevel, 'trace'));
 
-    describe('can log', function () {
+    describe('can log', () => {
       s2.error("some error");
       s2.error({
         key1: "value1"
@@ -51,7 +38,7 @@ describe('service', function () {
     });
   });
 
-  describe('derived service', function () {
+  describe('derived service', () => {
 
     class MyService extends Service {
       static get type() {
@@ -77,56 +64,48 @@ describe('service', function () {
       }
     };
 
-    describe('creation', function () {
+    describe('creation', () => {
       const s2 = new MyService({
         key3: "value3",
         key4: 4
       });
 
-      it('has a type', function () {
-        assert.equal(s2.type, 'my-service');
-      });
+      it('has a type', () => assert.equal(s2.type, 'my-service'));
 
-      it('has a toString', function () {
-        assert.equal(s2.toString(), 'my-service');
-      });
+      it('has a toString', () => assert.equal(s2.toString(), 'my-service'));
 
-      it('has values', function () {
+      it('has values', () => {
         assert.equal(s2.key3, 'value3');
         assert.equal(s2.key4, 4);
       });
     });
 
-    /*
-        describe('states', function () {
-          const s1 = new MyService({
-            key1: "value1",
-            key2: 2,
-          });
+    describe('states', () => {
+      const s1 = new MyService({
+        key1: "value1",
+        key2: 2,
+      });
 
-          it('can be started', function (done) {
-            s1.start().then(() => {
-              assert.equal(s1.state, 'running');
-              done();
-            }, done);
-          });
+      it('can be started', done => {
+        s1.start().then(() => {
+          assert.equal(s1.state, 'running');
+          done();
+        }, done);
+      });
 
-          const s2 = new MyService({
-            key1: "value1",
-            key2: 2,
-          });
+      const s2 = new MyService({
+        key1: "value1",
+        key2: 2,
+      });
 
-          it('derived state untouced', function () {
-            assert.equal(s2.state, 'stopped');
-          });
+      it('derived state untouced', () => assert.equal(s2.state, 'stopped'));
 
-          it('can be stopped', function (done) {
-            s1.stop().then(() => {
-              assert.equal(s1.state, 'stopped');
-              done();
-            }, done);
-          });
-        });
-        */
+      it('can be stopped', done => {
+        s1.stop().then(() => {
+          assert.equal(s1.state, 'stopped');
+          done();
+        }, done);
+      });
+    });
   });
 });
