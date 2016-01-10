c6917eff0dc082900d4844f73c348af446559cda
Markus Felten <Markus.Felten@gmx.de>
2016-01-10 16:13:51 +0100

    feat: apply several mixins LogLevel and StateTransition

--- tests/service_test.js
+++ tests/service_test.js
@@ -28,88 +28,105 @@ describe('service', function () {
       assert.equal(s1.state, 'stopped');
     });
 
-    it('has values', function () {
-      assert.equal(s1.key1, 'value1');
-      assert.equal(s1.key2, 2);
-    });
-
     it('has default logLevel info', function () {
       assert.equal(s1.logLevel, 'info');
     });
   });
 
-  /*
-    describe('creation with logLevel', function () {
-      const s2 = new Service({
-        key1: "value1",
-        logLevel: "trace"
-      });
+  describe('creation with logLevel', function () {
+    const s2 = new Service({
+      key1: "value1",
+      logLevel: "trace"
+    });
 
-      it('has given logLevel', function () {
-        assert.equal(s2.logLevel, 'trace');
-      });
+    it('has given logLevel', function () {
+      assert.equal(s2.logLevel, 'trace');
+    });
 
-      describe('can log', function () {
-        s2.error("some error");
-        s2.error({
-          key1: "value1"
-        });
+    describe('can log', function () {
+      s2.error("some error");
+      s2.error({
+        key1: "value1"
       });
     });
-  */
+  });
 
+  describe('derived service', function () {
 
-  /*
-    describe('derived creation', function () {
-      const s2 = service.createService('myService2', {
-        name: "some name",
+    class MyService extends Service {
+      static get type() {
+        return "my-service";
+      }
+      get type() {
+        return MyService.type;
+      }
+
+      constructor(config) {
+        super(config);
+
+        Object.defineProperty(this, 'key3', {
+          value: config.key3
+        });
+        Object.defineProperty(this, 'key4', {
+          value: config.key4
+        });
+      }
+
+      _start() {
+        return new Promise((f, r) => setTimeout(() => f(this), 10));
+      }
+    };
+
+    describe('creation', function () {
+      const s2 = new MyService({
         key3: "value3",
         key4: 4
-      }, s1);
+      });
 
-      it('has a name', function () {
-        assert.equal(s2.name, 'myService2');
+      it('has a type', function () {
+        assert.equal(s2.type, 'my-service');
       });
 
       it('has a toString', function () {
-        assert.equal(s2.toString(), 'myService2');
+        assert.equal(s2.toString(), 'my-service');
       });
 
       it('has values', function () {
-        assert.equal(s2.key1, 'value1');
-        assert.equal(s2.key2, 2);
         assert.equal(s2.key3, 'value3');
         assert.equal(s2.key4, 4);
       });
     });
 
-    describe('states', function () {
-      const s1 = service.createService('myService', {
-        key1: "value1",
-        key2: 2,
-        _start() {
-          return new Promise((f, r) => setTimeout(() => f(this), 10));
-        }
-      });
+    /*
+        describe('states', function () {
+          const s1 = new MyService({
+            key1: "value1",
+            key2: 2,
+          });
 
-      it('can be started', function (done) {
-        s1.start().then(() => {
-          assert.equal(s1.state, 'running');
-          done();
-        }, done);
-      });
+          it('can be started', function (done) {
+            s1.start().then(() => {
+              assert.equal(s1.state, 'running');
+              done();
+            }, done);
+          });
 
-      const s2 = service.createService('myService2', {}, s1);
-      it('derived state untouced', function () {
-        assert.equal(s2.state, 'stopped');
-      });
+          const s2 = new MyService({
+            key1: "value1",
+            key2: 2,
+          });
 
-      it('can be stopped', function (done) {
-        s1.stop().then(() => {
-          assert.equal(s1.state, 'stopped');
-          done();
-        }, done);
-      });
-    });
-    */
+          it('derived state untouced', function () {
+            assert.equal(s2.state, 'stopped');
+          });
+
+          it('can be stopped', function (done) {
+            s1.stop().then(() => {
+              assert.equal(s1.state, 'stopped');
+              done();
+            }, done);
+          });
+        });
+        */
+  });
 });
