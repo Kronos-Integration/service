d9c0bb6006da57942d06dacb0e20e9e5adc5489b
Markus Felten <Markus.Felten@gmx.de>
2016-01-11 22:07:54 +0100

    wip

--- index.js
+++ index.js
@@ -6,3 +6,5 @@
 exports.Service = require('./lib/Service');
 exports.ServiceConsumerMixin = require('./lib/ServiceConsumerMixin');
 exports.ServiceProviderMixin = require('./lib/ServiceProviderMixin');
+exports.ServiceProviderMixin = require('./lib/ServiceProviderMixin');
+exports.defineRegistrarProperties = require('./lib/RegistrarMixin').defineRegistrarProperties;
