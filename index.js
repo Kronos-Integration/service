d19ead179e985f28a8d5bafe3c24f85ce24a120c
Markus Felten <Markus.Felten@gmx.de>
2016-02-02 18:02:06 +0100

    fix: export Logger and Config Service

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 "use strict";
 
 exports.Service = require('./lib/Service');
+exports.Logger = require('./lib/ServiceLogger');
+exports.Config = require('./lib/ServiceConfig');
 exports.ServiceConsumerMixin = require('./lib/ServiceConsumerMixin');
 exports.ServiceProviderMixin = require('./lib/ServiceProviderMixin');
 exports.EndpointsMixin = require('./lib/EndpointsMixin');
