4d9ecf51b725537015c0bfb1ab77a87734711710
Markus Felten <Markus.Felten@gmx.de>
2016-02-04 21:00:35 +0100

    feat(service): by default ServiceProviderMixin now provides a config and a logger service

--- tests/service_provider_test.js
+++ tests/service_provider_test.js
@@ -27,37 +27,43 @@ class ServiceTest extends Service {
   }
 
   configure(config) {
+    this.trace(config);
     Object.assign(this, config);
     return this.restartIfRunning();
   }
 }
 
 describe('service provider', () => {
-  const sp = new ServiceProvider();
-  const serviceConfig = sp.registerService(new ServiceConfig({}, sp));
-  sp.registerService(new ServiceLogger({}));
-  sp.registerService(new ServiceTest({}));
+  const sp = new ServiceProvider({});
 
   describe('initial setup', () => {
     it('config service', () => assert.equal(sp.services.config.name, 'config'));
     it('logger service', () => assert.equal(sp.services.logger.name, 'logger'));
   });
 
-  describe('change', () => {
-    it('change', done => {
-      sp.services.config.endpoints.config.receive({
-        "config": {},
-        "test": {
-          key1: 4711,
-          key2: "2"
-        }
-      }).then(r => {
-        assert.equal(sp.services.test.key1, 4711);
-        done();
-      }).
-      catch(e => {
-        console.log(e);
-      });
+  describe('additional service', () => {
+    sp.registerService(new ServiceTest({}));
+    it('test service', () => assert.equal(sp.services.test.name, 'test'));
+
+    describe('configure service', () => {
+      it('direct', () => sp.services.test.configure({
+        key: "new value"
+      }).then(() =>
+        assert.equal(sp.services.test.key, "new value")
+      ));
+
+      it('send change request over config service', () =>
+        sp.services.config.endpoints.config.receive({
+          "config": {},
+          "unknown": {},
+          "test": {
+            key1: 4711,
+            key2: "2"
+          }
+        }).then(() =>
+          assert.equal(sp.services.test.key1, 4711)
+        )
+      );
     });
   });
 });
