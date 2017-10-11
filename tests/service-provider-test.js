0a2369d38f7b50a17cf6beaad8135ff7583271d4
Markus Felten <Markus.Felten@gmx.de>
2017-10-11 04:13:57 +0200

    test: service now provides description output

--- tests/service-provider-test.js
+++ tests/service-provider-test.js
@@ -9,7 +9,10 @@ const chai = require('chai'),
   should = chai.should(),
   endpoint = require('kronos-endpoint'),
   {
-    Service, ServiceProviderMixin, ServiceLogger, ServiceConfig
+    Service,
+    ServiceProviderMixin,
+    ServiceLogger,
+    ServiceConfig
   } = require('../dist/module');
 
 class ServiceProvider extends ServiceProviderMixin(Service) {
@@ -35,23 +38,29 @@ class ServiceTest extends Service {
 }
 
 describe('service provider', () => {
-  const sp = new ServiceProvider([{
-    name: 'a'
-  }, {
-    name: 'test',
-    key3: 3
-  }]);
+  const sp = new ServiceProvider([
+    {
+      name: 'a'
+    },
+    {
+      name: 'test',
+      key3: 3
+    }
+  ]);
 
   describe('initial setup', () => {
     describe('config service', () => {
       it('present', () => assert.equal(sp.services.config.name, 'config'));
-      it('preserved initial config', () => assert.deepEqual(Object.keys(sp.services.config.preservedConfigs), [
-        'a', 'test'
-      ]));
+      it('preserved initial config', () =>
+        assert.deepEqual(Object.keys(sp.services.config.preservedConfigs), [
+          'a',
+          'test'
+        ]));
     });
 
     it('logger service', () => assert.equal(sp.services.logger.name, 'logger'));
-    it('can be started', () => sp.start().then(() => assert.equal(sp.state, 'running')));
+    it('can be started', () =>
+      sp.start().then(() => assert.equal(sp.state, 'running')));
     it('service provider service', () => assert.equal(sp.services.a.name, 'a'));
   });
 
@@ -71,179 +80,214 @@ describe('service provider', () => {
 
     describe('info', () => {
       it('has response', () =>
-        testEndpoint.receive({
-          action: 'list'
-        }).then(r =>
-          assert.deepEqual(r.sort((a, b) => a.name.localeCompare(b.name)), [{
-            endpoints: {},
-            name: 'a',
-            type: 'service-provider'
-          }, {
-            endpoints: {
-              config: { in : true
+        testEndpoint
+          .receive({
+            action: 'list'
+          })
+          .then(r =>
+            assert.deepEqual(r.sort((a, b) => a.name.localeCompare(b.name)), [
+              {
+                endpoints: {},
+                name: 'a',
+                type: 'service-provider'
+              },
+              {
+                endpoints: {
+                  config: {
+                    in: true
+                  }
+                },
+                name: 'config',
+                type: 'config'
+              },
+              {
+                endpoints: {
+                  log: {
+                    in: true
+                  }
+                },
+                name: 'logger',
+                type: 'logger'
               }
-            },
-            name: 'config',
-            type: 'config'
-          }, {
-            endpoints: {
-              log: { in : true
-              }
-            },
-            name: 'logger',
-            type: 'logger'
-          }])
-        )
-      );
+            ])
+          ));
     });
     describe('get', () => {
       it('has response ', () =>
-        testEndpoint.receive({
-          action: 'get',
-          service: 'logger',
-          options: {
-            includeRuntimeInfo: true,
-            includeDefaults: true,
-            includeConfig: true,
-            includeName: true
-          }
-        }).then(r => {
-          assert.deepEqual(r, {
-            endpoints: {
-              command: { in : true
+        testEndpoint
+          .receive({
+            action: 'get',
+            service: 'logger',
+            options: {
+              includeRuntimeInfo: true,
+              includeDefaults: true,
+              includeConfig: true,
+              includeName: true
+            }
+          })
+          .then(r => {
+            assert.deepEqual(r, {
+              description:
+                'This service is the base class for service implementations',
+              endpoints: {
+                command: {
+                  in: true
+                },
+                config: {
+                  in: true
+                },
+                log: {
+                  in: true
+                }
               },
-              config: { in : true
+              timeout: {
+                start: 5
               },
-              log: { in : true
-              }
-            },
-            timeout: {
-              start: 5
-            },
-            logLevel: 'info',
-            state: 'running',
-            name: 'logger',
-            type: 'logger'
-          });
-        })
-      );
+              logLevel: 'info',
+              state: 'running',
+              name: 'logger',
+              type: 'logger'
+            });
+          }));
     });
 
     describe('start', () => {
       it('is running', () =>
-        testEndpoint.receive({
-          action: 'start',
-          service: 'logger'
-        }).then(r => {
-          assert.equal(r.state, 'running');
-        })
-      );
+        testEndpoint
+          .receive({
+            action: 'start',
+            service: 'logger'
+          })
+          .then(r => {
+            assert.equal(r.state, 'running');
+          }));
     });
     describe('stop', () => {
       it('is stopped', () =>
-        testEndpoint.receive({
-          action: 'stop',
-          service: 'logger'
-        }).then(r => {
-          assert.equal(r.state, 'stopped');
-        })
-      );
+        testEndpoint
+          .receive({
+            action: 'stop',
+            service: 'logger'
+          })
+          .then(r => {
+            assert.equal(r.state, 'stopped');
+          }));
     });
 
     describe('restart', () => {
       it('is running', () =>
-        testEndpoint.receive({
-          action: 'restart',
-          service: 'logger'
-        }).then(r => {
-          assert.equal(r.state, 'running');
-        })
-      );
+        testEndpoint
+          .receive({
+            action: 'restart',
+            service: 'logger'
+          })
+          .then(r => {
+            assert.equal(r.state, 'running');
+          }));
     });
 
     describe('several restarts', () => {
       it('is running', () =>
-        testEndpoint.receive([{
-          action: 'restart',
-          service: 'logger'
-        }, {
-          action: 'restart',
-          service: 'logger'
-        }]).then(r => {
-          assert.equal(r[1].state, 'running');
-        })
-      );
+        testEndpoint
+          .receive([
+            {
+              action: 'restart',
+              service: 'logger'
+            },
+            {
+              action: 'restart',
+              service: 'logger'
+            }
+          ])
+          .then(r => {
+            assert.equal(r[1].state, 'running');
+          }));
     });
 
     describe('restart unknown service', () => {
       it('is running', () =>
-        testEndpoint.receive({
-          action: 'restart',
-          service: 'invalid'
-        }).then(r => {
-          assert.equal(r.state, 'xxrunning');
-        }, e => {
-          assert.ok(true);
-        })
-      );
+        testEndpoint
+          .receive({
+            action: 'restart',
+            service: 'invalid'
+          })
+          .then(
+            r => {
+              assert.equal(r.state, 'xxrunning');
+            },
+            e => {
+              assert.ok(true);
+            }
+          ));
     });
 
     describe('unknown command', () => {
       it('rejects', () =>
-        testEndpoint.receive({
-          action: 'unknown',
-          service: 'logger'
-        }).then(r => {
-          assert.equal(r.state, 'xxrunning');
-        }, e => {
-          assert.ok(true);
-        })
-      );
+        testEndpoint
+          .receive({
+            action: 'unknown',
+            service: 'logger'
+          })
+          .then(
+            r => {
+              assert.equal(r.state, 'xxrunning');
+            },
+            e => {
+              assert.ok(true);
+            }
+          ));
     });
   });
 
   describe('additional service', () => {
     before(() => {
       sp.registerService(new ServiceTest({}, sp));
-      sp.registerService(new ServiceTest({
-        name: 't2'
-      }, sp));
+      sp.registerService(
+        new ServiceTest(
+          {
+            name: 't2'
+          },
+          sp
+        )
+      );
     });
 
     it('test service', () => assert.equal(sp.services.test.name, 'test'));
 
     describe('configure service', () => {
-      it('direct', () => sp.services.test.configure({
-        key: 'new value'
-      }).then(() =>
-        assert.equal(sp.services.test.key, 'new value')
-      ));
+      it('direct', () =>
+        sp.services.test
+          .configure({
+            key: 'new value'
+          })
+          .then(() => assert.equal(sp.services.test.key, 'new value')));
 
       it('send change request over config service', () =>
-        sp.services.config.endpoints.config.receive([{
-          name: 'config'
-        }, {
-          name: 'unknown'
-        }, {
-          name: 'test',
-          key1: 4711,
-          key2: '2'
-        }]).then(() =>
-          assert.equal(sp.services.test.key1, 4711)
-        )
-      );
+        sp.services.config.endpoints.config
+          .receive([
+            {
+              name: 'config'
+            },
+            {
+              name: 'unknown'
+            },
+            {
+              name: 'test',
+              key1: 4711,
+              key2: '2'
+            }
+          ])
+          .then(() => assert.equal(sp.services.test.key1, 4711)));
     });
 
     it('can be unregistered', () =>
-      sp.unregisterService('t2').then(s => assert.isUndefined(sp.services.t2))
-    );
+      sp.unregisterService('t2').then(s => assert.isUndefined(sp.services.t2)));
   });
 
   describe('declare service', () => {
     describe('with type', () => {
       before(() => {
-        setTimeout(() =>
-          sp.registerServiceFactory(ServiceTest), 30);
+        setTimeout(() => sp.registerServiceFactory(ServiceTest), 30);
 
         // force pending promises
         /*
@@ -259,55 +303,66 @@ describe('service provider', () => {
       });
 
       it('can be declared', () =>
-        sp.declareService({
-          name: 's2',
-          type: 'test',
-          key: 1
-        }, true).then(
-          s => { 
+        sp
+          .declareService(
+            {
+              name: 's2',
+              type: 'test',
+              key: 1
+            },
+            true
+          )
+          .then(s => {
             assert.equal(s.name, 's2');
             assert.equal(s.type, 'test');
-          }
-        )
-      );
+          }));
 
       it('can be declared again', () =>
-        sp.declareService({
-          name: 's2',
-          type: 'test',
-          key: 2
-        }, true).then(
-          s => {
+        sp
+          .declareService(
+            {
+              name: 's2',
+              type: 'test',
+              key: 2
+            },
+            true
+          )
+          .then(s => {
             assert.equal(s.name, 's2');
             assert.equal(s.key, 2);
-          }
-        )
-      );
+          }));
     });
 
     xdescribe('without type', () => {
-      const sp = new ServiceProvider([{}, {
-        name: 'test',
-        value: 77
-      }]);
+      const sp = new ServiceProvider([
+        {},
+        {
+          name: 'test',
+          value: 77
+        }
+      ]);
 
-      sp.declareService({
-        name: 'test'
-      }, true);
+      sp.declareService(
+        {
+          name: 'test'
+        },
+        true
+      );
 
-      setTimeout(() =>
-        sp.registerServiceFactory(ServiceTest), 50);
+      setTimeout(() => sp.registerServiceFactory(ServiceTest), 50);
 
       it('can be declared', () =>
-        sp.declareService({
-          name: 'test'
-        }, true).then(
-          s => {
+        sp
+          .declareService(
+            {
+              name: 'test'
+            },
+            true
+          )
+          .then(s => {
             assert.equal(s.name, 'test');
             //assert.equal(s.value, 77);
-          }
-        )
-      );
+          }));
     });
   });
 });
