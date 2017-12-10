bd9110dd9afe7a78e2ba9980e87857f46be04854
Markus Felten <Markus.Felten@gmx.de>
2017-12-10 16:08:25 +0100

    fix: export InterceptorProviderMixin

--- src/module.js
+++ src/module.js
@@ -3,11 +3,13 @@ import ServiceLogger from './service-logger';
 import ServiceConfig from './service-config';
 import { defineServiceConsumerProperties } from './service-consumer-mixin';
 import ServiceProviderMixin from './service-provider-mixin';
+import InterceptorProviderMixin from './interceptor-provider-mixin';
 import EndpointsMixin from './endpoints-mixin';
 
 export {
   EndpointsMixin,
   ServiceProviderMixin,
+  InterceptorProviderMixin,
   defineServiceConsumerProperties,
   ServiceConfig,
   ServiceLogger,
