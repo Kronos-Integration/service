c5a42d81942683670514a7f504a7bd2705cd1ae9
Markus Felten <Markus.Felten@gmx.de>
2019-11-24 16:17:24 +0100

    test: correct fixture

--- tests/service-provider-test.mjs
+++ tests/service-provider-test.mjs
@@ -114,7 +114,7 @@ test("service provider additional service logging", async t => {
   ]);
   //console.log(sp.services.logger.logEntries);
 
-  t.is(sp.services.logger.logEntries.length, 12);
+  t.is(sp.services.logger.logEntries.length, 14);
 });
 
 test("service provider additional service can be unregistered", async t => {
