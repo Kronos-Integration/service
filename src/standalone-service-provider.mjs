8536d65ea4a1665811d791041f012c57162be4ec
Markus Felten <Markus.Felten@gmx.de>
2019-11-25 14:33:50 +0100

    fix: name StandaloneServiceProvider

--- src/standalone-service-provider.mjs
+++ src/standalone-service-provider.mjs
@@ -5,5 +5,10 @@ import ServiceProviderMixin from './service-provider-mixin.mjs';
  * simple service manager (for examples and testing only)
  */
 export default class StandaloneServiceProvider extends ServiceProviderMixin(Service) {
-
+  /**
+   * @return {string} 'stantalone-provider'
+   */
+  static get name() {
+    return 'stantalone-provider';
+  }
 }
